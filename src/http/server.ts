73e0c9c6c84c494f1b3bb4a3fda7b30786930701
Clayton Rafael <clayton.rssouza@gmail.com>
2024-02-06 08:45:33 -0300

    feat: config http server

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,11 @@
-console.log("Starting")
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get('/hello', () => {
+   return 'Hello NLW!'; 
+});
+
+app.listen({ port: 3333 }).then(() => {
+    console.log("HTTP server is running!")
+});
