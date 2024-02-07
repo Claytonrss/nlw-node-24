33841351f1dadab794c5c3178c35f457c6b84738
Clayton Rafael <clayton.rssouza@gmail.com>
2024-02-06 21:45:03 -0300

    feat: implement create route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,30 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import { z } from "zod";
+
+const prisma = new PrismaClient();
 
 const app = fastify();
 
-app.get('/hello', () => {
-   return 'Hello NLW!'; 
+app.post("/polls", async (request, reply) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+
+  const { title } = createPollBody.parse(request.body);
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+  return reply.status(201).send({ pollId: poll.id });
+});
+
+app.get("/hello", () => {
+  return "Hello NLW!";
 });
 
 app.listen({ port: 3333 }).then(() => {
-    console.log("HTTP server is running!")
+  console.log("HTTP server is running!");
 });
